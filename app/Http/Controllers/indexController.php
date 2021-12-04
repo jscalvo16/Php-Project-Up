d8a93ed46bb0836327c7e5bb9bc0c7ed55592228
Nicolas Rosero <nicolass2310@gmail.com>
2021-12-03 22:03:46 -0500

    Perfil arreglado

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Ficha;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class indexController extends Controller
 {
@@ -16,7 +18,16 @@ class indexController extends Controller
     // Mostrar el perfil del usuario
     public function mostrarPerfil()
     {
-        $fichas = Ficha::all();
+        if(Auth::user()->FkIdRol == 1 || Auth::user()->FkIdRol == 2){
+            $fichas = DB::table('ficha')->
+            leftJoin('usuafich', 'usuafich.FkIdFicha', '=', 'ficha.IdFicha')->
+            select('ficha.IdFicha as IdFicha', 'ficha.NumbFich as NumbFich', 'ficha.Trimestre as Trimestre', 'ficha.InicEtapElec as InicEtapElec', 'ficha.FinEtapElec as FinEtapElec', 'ficha.JornFicha as JornFicha')->
+            where('usuafich.FkIdUsua', '=', Auth::user()->IdUsua)->
+            get();
+        }else{
+            $fichas = Ficha::all();
+        }
+
         return view('usuarios.perfil', compact('fichas'));
     }
 }
